d56b7bc94174be55257813392deeaae068401129
Lázaro Amor Sevilla <lazaroesunagranpersona@gmail.com>
2022-02-22 00:23:04 +0100

    Add tailwindcss

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
+import '../styles/global.css'
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -8,7 +9,7 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
+    <h1 className="text-3xl font-bold underline">Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <StaticImage
